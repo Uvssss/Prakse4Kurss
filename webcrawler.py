80c61f54a315341a59572a308a8863781cba3f08
Uvssss <uviskletnieks27@gmail.com>
2023-01-23 12:31:30 +0200

    got data to json

--- webcrawler.py
+++ webcrawler.py
@@ -1,7 +1,8 @@
-import requests
-import lxml
-from bs4 import BeautifulSoup
 from urllib.request import urlopen
-html = urlopen("https://www.nordpoolgroup.com/api/marketdata/page/59?currency=,,,EUR").read()
-print(html)
-
+import json
+html = urlopen("https://www.nordpoolgroup.com/api/marketdata/page/59?currency=,,,EUR")
+data_json = json.loads(html.read())
+print(data_json)
+# json_object = json.dumps(data_json, indent=4)
+# with open("json/data.json", "w") as outfile:
+#     outfile.write(json_object)
