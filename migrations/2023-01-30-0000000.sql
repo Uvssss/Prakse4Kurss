875d11c2bf9e760d615783c16ca068ba84bb767b
H1TMAN4U <andris.artejevs@valmierastehnikums.lv>
2023-01-30 16:26:57 +0200

    fixed foreign key and renamed items table name

--- migrations/2023-01-30-0000000.sql
+++ migrations/2023-01-30-0000000.sql
@@ -1,4 +1,4 @@
-create table item(
+create table items(
 	`id` int not null unique auto_increment,
     `name` varchar(45) not null
 );
--- migrations/2023-01-30-0000003.sql
+++ migrations/2023-01-30-0000003.sql
@@ -1,8 +1,8 @@
 create table consumption(
 	`id` int not null unique auto_increment,
-    `starttime` varchar(45) not nullid
+    `startime` varchar(45) not nullid
     `endtime` varchar(45) not null
     `items_id` int not null,
-    foreign_key(items_id) references items(id)
+    foreign key(items_id) references items(id)
     
 );
