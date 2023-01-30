1a9ad539cb1024cc505ccb82256475986548e2df
H1TMAN4U <andris.artejevs@valmierastehnikums.lv>
2023-01-30 16:50:24 +0200

    yes

--- migrations/2023-01-30-0000003.sql
+++ migrations/2023-01-30-0000003.sql
@@ -1,7 +1,7 @@
 create table consumption(
 	`id` int not null unique auto_increment,
-    `startime` varchar(45) not nullid
-    `endtime` varchar(45) not null
+    `startime` varchar(45) not null,
+    `endtime` varchar(45) not null,
     `items_id` int not null,
     foreign key(items_id) references items(id)
     
