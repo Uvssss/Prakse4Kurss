36449927a26f5bdf567288e357834bcaec27664f
Uvssss <uviskletnieks27@gmail.com>
2023-01-31 13:33:17 +0200

    syntax

--- experement.py
+++ experement.py
@@ -4,11 +4,11 @@ import time
 from main import *
 from worker import *
 
-schedule.every().day.at("00:01").do(insert)
+schedule.every().day.at("00:01").do(insert())
 
 
 
-schedule.every().hour.do(create_consumtion(startime,endtime))
+schedule.every().hour.do(create_consumtion())
 
 
 while True:
