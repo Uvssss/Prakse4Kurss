948944e88d07e255835d318d71f44b9022d2011a
H1TMAN4U <andris.artejevs@valmierastehnikums.lv>
2023-01-31 09:25:49 +0200

    2023-01-30-0000001

--- migrations/2023-01-30-0000000.sql
+++
@@ -1,4 +0,0 @@
-create table items(
-	`id` int not null unique auto_increment,
-    `name` varchar(45) not null
-);
--- migrations/2023-01-30-0000003.sql
+++
@@ -1,8 +0,0 @@
-create table consumption(
-	`id` int not null unique auto_increment,
-    `startime` varchar(45) not null,
-    `endtime` varchar(45) not null,
-    `items_id` int not null,
-    foreign key(items_id) references items(id)
-    
-);
