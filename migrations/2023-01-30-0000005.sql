8c1e3210f6fa7a94accebc3e21848f6e417f97be
Uvssss <uviskletnieks27@gmail.com>
2023-01-31 09:27:21 +0200

    yes

--- migrations/2023-01-30-0000005.sql
+++ migrations/2023-01-30-0000005.sql
@@ -1,9 +1,7 @@
 create table battery_info(
-    `id` int not null auto_increment,
-	`startime` varchar(100)  not null,
-    `endtime` varchar(100)  not null,
+    `id` int not null auto_increment primary key,
+    `name` varchar(45) not null
     `max_capacity` float not null,
     `current_capacity` float not null,
     `charge_power` float not null,
-    `name` varchar(45) not null
 );
