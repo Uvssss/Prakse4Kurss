835ffab47205881fe88ca5d4239a66249e68d880
Uvssss <uviskletnieks27@gmail.com>
2023-02-01 13:10:30 +0200

    db additions

--- migrations/2023-01-30-00000070.sql
+++ migrations/2023-01-30-00000070.sql
@@ -2,6 +2,7 @@ create table total_cost(
     startime varchar(100) not null,
     endtime varchar(100) not null,
     price float not null,
-    consumption float not null
+    consumption float not null,
+    expenses float not null
     
 );
