29472e19c45986884d1451f713922c3f363ddace
Uvssss <uviskletnieks27@gmail.com>
2023-02-01 09:19:57 +0200

    changes

--- migrations/2023-01-30-0000004.sql
+++ migrations/2023-01-30-0000004.sql
@@ -1,6 +1,5 @@
 create table battery(
     id int not null,
-    `name` varchar(45) not null,
     `max_capacity` float not null,
     `current_capacity` float not null,
     `charge_power` float not null,
